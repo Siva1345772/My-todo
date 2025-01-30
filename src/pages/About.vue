b2f93c340384b620c39994b97c00220ba2f310cf
Siva1345772 <119560735+Siva1345772@users.noreply.github.com>
2025-01-30 20:40:58 +0530

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <h1>About</h1>
-        <p>This is the about page</p>
-    </div>
+  <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to My Todo</span>
+    </template>
+
+    <v-card-text class="bg-surface-light pt-4">
+      Managing your tasks efficiently is the key to staying productive and organized. With this to-do list, you can keep track of your daily, weekly, and long-term goals in one convenient place.
+    </v-card-text>
+    <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        size="small"
+        variant="plain"
+      ></v-btn>
+  </v-card>
 </template>
+
+<script setup>
+  icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ]
+</script>
